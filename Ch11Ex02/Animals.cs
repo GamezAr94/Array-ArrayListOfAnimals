6c953fd57f534609bb540d144fab509a1054b019
GamezArt94 <46079765+GamezAr94@users.noreply.github.com>
2020-02-25 14:36:25 -0800

    Create Animals class
    Adding A DictionaryBase collection

---
+++ Ch11Ex02/Animals.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ch11Ex02
+{
+    class Animals : DictionaryBase
+    {
+        public void Add(string newID, Animal newAnimal)
+        {
+            Dictionary.Add(newID, newAnimal);
+        }
+        public void Remove(string animalID)
+        {
+            Dictionary.Remove(animalID);
+        }
+        public Animals() { }
+        public Animal this[string animalID]
+        {
+            get => (Animal)Dictionary[animalID];
+            set => Dictionary[animalID] = value;
+        }
+    }
+}
